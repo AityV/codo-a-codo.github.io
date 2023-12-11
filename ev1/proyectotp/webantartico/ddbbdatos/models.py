7025bba7cc97c71bd3898aac2947ae7788910a79
Emilce Cano <emilce.cano95@gmail.com>
2023-12-11 03:27:25 -0300

    update

--- ev1/proyectotp/webantartico/ddbbdatos/models.py
+++ ev1/proyectotp/webantartico/ddbbdatos/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
 class Persona(models.Model):
     IdPersona=models.IntegerField()
     nombre = models.CharField(max_length=50)
@@ -9,10 +8,10 @@ class Persona(models.Model):
     correo = models.CharField(max_length=50)
     IdTelefono = models.IntegerField()
     
-
     class Telefono(models.Model):
         IdTelefono = models.IntegerField()
         telefono = models.CharField(max_length=15)
         codigoArea = models.CharField(max_length=10)
+        
        
    
