34fb52adb6f3d250942cdf6fd5d8d04110393810
yexicah <146391427+yexicah@users.noreply.github.com>
2023-12-10 23:56:29 -0300

    [Feat] desarrollo BE con entorno virtual

---
+++ ev1/proyectotp/webantartico/core/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "core/base.html")
+
+def destinations(request):
+    return render(request, "core/destinations.html")
+
+def packages(request):
+    return render(request, "core/packages.html")
+
+def contact(request):
+    return render(request, "core/contact.html")
+
+def construction(request):
+    return render(request, "core/construction.html")
