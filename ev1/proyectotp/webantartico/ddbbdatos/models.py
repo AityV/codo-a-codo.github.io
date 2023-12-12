1ab679c68b734b369dde918d61b8d84083fb71b5
Emilce Cano <emilce.cano95@gmail.com>
2023-12-12 02:01:21 -0300

    update mysql

--- ev1/proyectotp/webantartico/ddbbdatos/models.py
+++ ev1/proyectotp/webantartico/ddbbdatos/models.py
@@ -1,18 +1,26 @@
 from django.db import models
-import sqlite3
+import mysql.connector
 
+conexion = mysql.connector.connect(user='root', 
+                                   password='1234', 
+                                   host='localhost', 
+                                   database='contact', 
+                                   port = '3306')
+
+print(conexion)
+# import sqlite3
 # Create your models here.
-class Persona(models.Model):
-    IdPersona=models.IntegerField()
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
-    correo = models.CharField(max_length=50)
-    IdTelefono = models.IntegerField(Telefono, on_delete=models.CASCADE)
+# class Persona(models.Model):
+#     IdPersona=models.IntegerField()
+#     nombre = models.CharField(max_length=50)
+#     apellido = models.CharField(max_length=50)
+#     correo = models.CharField(max_length=50)
+#     IdTelefono = models.IntegerField()
     
-    class Telefono(models.Model):
-        IdTelefono = models.IntegerField()
-        telefono = models.CharField(max_length=15)
-        codigoArea = models.CharField(max_length=10)
+#     class Telefono(models.Model):
+#         IdTelefono = models.IntegerField()
+#         telefono = models.CharField(max_length=15)
+#         codigoArea = models.CharField(max_length=10)
         
        
    
--- ev1/proyectotp/webantartico/webantartico/settings.py
+++ ev1/proyectotp/webantartico/webantartico/settings.py
@@ -75,10 +75,18 @@ WSGI_APPLICATION = 'webantartico.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "read_default_file": "/path/to/my.cnf",
+        },
     }
 }
 
