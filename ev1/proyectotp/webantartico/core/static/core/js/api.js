334a363024bc48f2227f50971b589b136e40f9b7
yexicah <146391427+yexicah@users.noreply.github.com>
2023-12-11 23:49:42 -0300

    [Fix] Correccion de detalles y pag construction

--- ev1/proyectotp/webantartico/core/static/core/js/api.js
+++ ev1/proyectotp/webantartico/core/static/core/js/api.js
@@ -1,13 +1,13 @@
 // Creando el mapa
 function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: -34.5859106, lng: -58.3789122 },
-      zoom: 17
-    });
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.5859106, lng: -58.3789122 },
+    zoom: 17
+  });
 
-    const marcador = new google.maps.Marker({
-      position:{ lat: -34.5859106, lng: -58.3789122 },
-      map: map,
-      title: "Codo a Codo"
-    });
-  }
+  const marcador = new google.maps.Marker({
+    position:{ lat: -34.5859106, lng: -58.3789122 },
+    map: map,
+    title: "Codo a Codo"
+  });
+}
